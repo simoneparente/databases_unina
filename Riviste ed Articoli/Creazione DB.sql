f737f1eb5ed5f5add50f3e0be1fd7d3e0fbea942
Mario Penna <mario.penna00@gmail.com>
2022-12-14 19:10:59 +0100

    bho

--- Riviste ed Articoli/Creazione DB.sql
+++ Riviste ed Articoli/Creazione DB.sql
@@ -79,3 +79,33 @@ CREATE TABLE r.ParoleChiave
 
 ------------------------------------------------------------------------------------------------------------------------
 --Trigger 1
+CREATE OR REPLACE FUNCTION r.trig1() RETURNS TRIGGER AS $$
+    DECLARE
+        i INTEGER := 0;
+        n INTEGER := (SELECT COUNT(*) FROM r.parolechiave NATURAL JOIN fascicolo);
+        word r.parolechiave.parola%TYPE;
+        cursore CURSOR FOR SELECT p.parola FROM r.parolechiave as p NATURAL JOIN fascicolo as f WHERE r.fascicolo.codf=NEW.codf;
+
+    BEGIN
+        OPEN cursore;
+
+        WHILE (i<n) LOOP
+            FETCH cursore INTO word;
+            IF (NEW.sommario LIKE '%'|| word || '%') THEN
+                INSERT INTO r.descrizione(parola, doi)
+                values (word, NEW.doi);
+            END IF;
+            i = i + 1;
+        END LOOP ;
+        CLOSE cursore;
+        RETURN NEW;
+    END;
+$$LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER TRIGGER_1  AFTER INSERT ON r.articolo
+FOR EACH ROW
+EXECUTE PROCEDURE r.trig1();
+
+------------------------------------------------------------------------------------------------------------------------
+--Trigger 2
