7019589c3882b4bc00e42c4d4827c5a2f89eac83
Simone Parente <parentesimone.01@gmail.com>
2022-12-14 19:37:51 +0100

    ordine db e insert
    aggiunte 2 insert perché fidarsi è bene ma non fidarsi è meglio

---
+++ esercizi RIVISTE/Creazione DB.sql
@@ -0,0 +1,78 @@
+DROP SCHEMA r CASCADE ;
+CREATE SCHEMA r;
+
+CREATE TABLE r.rivista (
+    isnn VARCHAR(32),
+    titolo VARCHAR(32),
+    editore VARCHAR(32),
+    periodicita VARCHAR(32),
+    CONSTRAINT pk_rivista PRIMARY KEY (isnn)
+);
+
+
+CREATE TABLE r.fascicolo(
+    CodF VARCHAR(32),
+    isnn VARCHAR(32),
+    numero int,
+    anno int,
+    CONSTRAINT pk_fascicolo PRIMARY KEY (CodF),
+    CONSTRAINT fk_fascicolo_rivista FOREIGN KEY (isnn) REFERENCES r.rivista(isnn)
+);
+CREATE TABLE r.articolo (
+    doi      VARCHAR(32),
+    CodF     VARCHAR(32),
+    titolo   VARCHAR(32),
+    autore   VARCHAR(32),
+    sommario VARCHAR(32),
+    PagI     INTEGER,
+    PagF     INTEGER,
+    CONSTRAINT pk_articolo         PRIMARY KEY (doi),
+    CONSTRAINT FK_ARTICOLO_RIVISTA FOREIGN KEY (CodF) REFERENCES r.fascicolo(CodF)
+);
+CREATE TABLE r.Profilo
+(
+    CodProfilo VARCHAR(32),
+    Tipo       VARCHAR(32),
+    MaxGiorno  INTEGER,
+    MaxMese    INTEGER,
+    CONSTRAINT PK_Profilo PRIMARY KEY (CodProfilo)
+);
+
+CREATE TABLE r.Utente
+(
+    CF          VARCHAR(32),
+    email       VARCHAR(32),
+    CodProfilo  VARCHAR(32),
+    Nome        VARCHAR(32),
+    Cognome     VARCHAR(32),
+    DataNascita DATE,
+    CONSTRAINT PK_Utente         PRIMARY KEY (CF),
+    CONSTRAINT FK_Utente_Profilo FOREIGN KEY (CodProfilo) REFERENCES r.Profilo (CodProfilo)
+);
+
+CREATE TABLE r.accesso(
+    CF   VARCHAR(32),
+    doi  VARCHAR(32),
+    data DATE,
+    CONSTRAINT pk_accesso          PRIMARY KEY (CF,doi,data),
+    CONSTRAINT fk_accesso_articolo FOREIGN KEY (doi) REFERENCES r.articolo(doi),
+    CONSTRAINT fk_accesso_utente   FOREIGN KEY (CF)  REFERENCES r.utente(CF)
+);
+
+
+CREATE TABLE r.Descrizione
+(
+    Parola VARCHAR(32),
+    Doi    VARCHAR(32),
+    CONSTRAINT PK_Descrizione             PRIMARY KEY (Parola,Doi),
+    CONSTRAINT FK_Descrizione_Articolodoc FOREIGN KEY (Doi) REFERENCES r.Articolo (Doi)
+);
+
+CREATE TABLE r.ParoleChiave
+(
+    Parola VARCHAR(32),
+    ISNN   VARCHAR(32),
+
+    CONSTRAINT PK_ParoleChiave PRIMARY KEY (Parola, ISNN),
+    CONSTRAINT FK_ParoleChiave_ FOREIGN KEY (ISNN) REFERENCES r.rivista(ISNN)
+);
---
+++ esercizi RIVISTE/Insert.sql
@@ -0,0 +1,21 @@
+--RIVISTA
+INSERT INTO r.rivista(ISNN)
+values ('rivista1'),
+       ('rivista2');
+
+--FASCICOLO
+INSERT INTO r.fascicolo(isnn, codf)
+values ('rivista1','fascicolo1'),
+       ('rivista2','fascicolo2');
+
+--PAROLECHIAVE
+INSERT INTO r.parolechiave(isnn, parola)
+values ('rivista1', 'silvio1'),
+       ('rivista1', 'salernitana1'),
+       ('rivista2', 'barra2'),
+       ('rivista2', 'prova2');
+
+--ARTICOLO
+INSERT INTO r.articolo(doi, codf, sommario)
+values ('doi1', 'fascicolo1', 'silvio1 barra2 salernitana1'),
+       ('doi2', 'fascicolo2', 'silvio1 barra2 prova2');
