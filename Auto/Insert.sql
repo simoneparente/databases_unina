154b53a55ccba3ad0c582ecc5227fb0787989aaa
Simone Parente <parentesimone.01@gmail.com>
2022-12-14 09:24:34 +0100

    Funziona Bene
    Co-Authored-By: Mario Penna <117465087+bickpenna@users.noreply.github.com>

--- Auto/Insert.sql
+++ Auto/Insert.sql
@@ -1,6 +1,7 @@
 
 
-INSERT INTO v.AUTO(targa, codfis, categoria) values (1, 1, 1);
+INSERT INTO v.AUTO(targa, codfis, categoria) values (1, 1, 1),
+                                                    (2, 2, 2);
 
 
 INSERT INTO v.PCHECK(puntocheck, velocitamax) values ('prova', 10);
@@ -12,5 +13,12 @@ INSERT INTO v.TARIFFE(ingresso, uscita, km, categoria, costo) values ('prova1','
 INSERT INTO v.VIAGGIO(km, tariffa, codiceviaggio, ingresso, datai, targa, orai) values (null, null, 1, 'prova', '2001-01-01', 1, '00:00');
 
 
-INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo) VALUES ('prova', 1, 11, '2001-01-01', '00:30');
-INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo) VALUES ('prova1', 1, 11, '2001-01-01', '00:30');
+INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo) VALUES ('prova', 1, 9, '2001-01-01', '00:30');
+INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo) VALUES ('prova1', 1, 11, '2001-01-01', '00:45');
+
+INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo)
+    VALUES  ('prova', '2', 11,'2007-07-07','00:45'),
+            ('prova1', '2', 9,'2007-07-07','00:40');
+
+INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo)
+    VALUES  ('prova1', '2', 11,'2007-12-07','01:50');
--- Auto/Trigger.sql
+++ Auto/Trigger.sql
@@ -3,10 +3,16 @@ $$
     DECLARE
         MAXvelocita v.pcheck.velocitaMAX%TYPE;
         BEGIN
-            SELECT velocitaMAX INTO MAXvelocita FROM v.pcheck WHERE puntocheck = NEW.puntocheck;
+            SELECT velocitaMAX INTO MAXvelocita
+                FROM v.pcheck WHERE puntocheck = NEW.puntocheck;
 
             IF NEW.velocita > MAXvelocita THEN
-                UPDATE v.check SET infrazione = TRUE WHERE puntocheck = NEW.puntocheck AND targa = NEW.targa;
+                UPDATE v.check
+                SET infrazione = TRUE WHERE puntocheck = NEW.puntocheck
+                    AND targa = NEW.targa
+                    AND velocita=NEW.velocita
+                    AND data=NEW.data
+                    AND tempo=NEW.tempo;
             END IF;
             RAISE NOTICE 'Infrazione: %', new.infrazione;
         RETURN NULL;
@@ -15,10 +21,10 @@ $$  LANGUAGE plpgsql;
 
 CREATE or REPLACE TRIGGER Infractions AFTER INSERT ON v.check
 FOR  ROW
-EXECUTE FUNCTION v.setInfraction();
+EXECUTE PROCEDURE v.setInfraction();
 
 INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo)
 values ('prova', '1', 9, '2001-01-01', '00:00:00'); -- non infrangono
 
 INSERT INTO v.CHECK(puntocheck, targa, velocita, data, tempo)
-values ('prova1', '1', 110, '2001-01-01', '00:00:00'); -- infrangono
+values ('prova', '1', 110, '2001-01-01', '00:00:10'); -- infrangono
