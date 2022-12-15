7f112fefc682266bc8008312be206bc7b1706f44
Mario Penna <mario.penna00@gmail.com>
2022-12-15 20:00:09 +0100

    Update Funzione ORA BASTA CI PROVO IO.sql

--- Riviste ed Articoli/Funzione ORA BASTA CI PROVO IO.sql
+++ Riviste ed Articoli/Funzione ORA BASTA CI PROVO IO.sql
@@ -1,6 +1,11 @@
 CREATE OR REPLACE PROCEDURE r.funzMario(stringa VARCHAR(1000)) AS $$
     DECLARE
         cursore_numPC CURSOR FOR SELECT d.doi, count(d.parola) FROM r.descrizione as d GROUP BY d.doi;
+        numPC INTEGER;
+    BEGIN
+        open cursore_numPC;
+        cursore_numPC INTO numPC;
+    end;
 
 
 $$
