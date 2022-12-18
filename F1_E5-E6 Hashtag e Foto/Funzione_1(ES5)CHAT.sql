38d6c022095ab65fa7047e637be230032376ebb5
Mario Penna <mario.penna00@gmail.com>
2022-12-18 21:00:45 +0100

    chat

---
+++ F1_E5-E6 Hashtag e Foto/Funzione_1(ES5)CHAT.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE PROCEDURE get_tags_by_album (p_CodA INTEGER)
+RETURNS TEXT AS
+$BODY$
+DECLARE
+  -- variabile per memorizzare i tag trovati
+  l_tags TEXT;
+BEGIN
+  -- inizializza la variabile
+  l_tags := '';
+
+  -- seleziona i tag dell'album specificato
+  SELECT DISTINCT parola
+  INTO l_tags
+  FROM TAGALBUM
+  WHERE CodA = p_CodA;
+
+  -- seleziona i tag degli album contenuti nell'album specificato
+  FOR rec IN (SELECT DISTINCT CodA
+              FROM INALBUM
+              WHERE CodP = p_CodA)
+  LOOP
+    -- richiama la procedura ricorsivamente per ogni album contenuto
+    l_tags := l_tags || ' ' || get_tags_by_album(rec.CodA);
+  END LOOP;
+
+  -- restituisce i tag trovati
+  RETURN l_tags;
+END;
+$BODY$
+LANGUAGE plpgsql;
