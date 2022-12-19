a4ae0b41655df842e36571d3f9a1337b0289bec2
Lorenzo Tecchia <rueful.chest_0f@icloud.com>
2022-12-19 15:50:37 +0100

    Simone sei una lota

--- F1_E5-E6 Hashtag e Foto/ES5_REC.sql
+++ F1_E5-E6 Hashtag e Foto/ES5_REC.sql
@@ -2,31 +2,36 @@ DROP TABLE f.temp;
 CREATE OR REPLACE PROCEDURE f.f_interna(tagalbum f.album.codA%TYPE)
 AS
 $$
-    DECLARE
+DECLARE
 
-    BEGIN
-        RAISE NOTICE 'ALbero passato(%)', tagalbum;
-        
+BEGIN
+    RAISE NOTICE 'ALbero passato(%)', tagalbum;
+
     INSERT INTO f.temp(coda) VALUES (tagalbum);
-    END;
+END;
 $$
-LANGUAGE plpgsql;
+    LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE f.f1_rec(input f.album.codA%TYPE) AS
 $$
-    DECLARE
-        figli_input CURSOR FOR ( SELECT coda FROM f.album WHERE inalbum=input);
-        figlio f.album.codA%TYPE;
-    BEGIN
-        CREATE TABLE f.TEMP(CodA INTEGER);
-        OPEN figli_input;
-        LOOP
+DECLARE
+    figli_input CURSOR FOR (SELECT coda
+                            FROM f.album
+                            WHERE inalbum = input);
+    figlio f.album.codA%TYPE;
+BEGIN
+    CREATE TABLE f.TEMP
+    (
+        CodA INTEGER
+    );
+    OPEN figli_input;
+    LOOP
         FETCH figli_input INTO figlio;
         CALL f.f_interna(figlio);
         EXIT WHEN NOT FOUND;
-        END LOOP;
-    END;
+    END LOOP;
+END;
 $$
-LANGUAGE plpgsql;
+    LANGUAGE plpgsql;
 
 CALL f.f1_rec(1);
--- F1_E5-E6 Hashtag e Foto/Funzione_2(ES6).sql
+++ F1_E5-E6 Hashtag e Foto/Funzione_2(ES6).sql
@@ -63,45 +63,55 @@ WHERE uri='uri1' OR uri='uri2' OR uri='uri3';
 CREATE OR REPLACE PROCEDURE f.prova1(input VARCHAR(500)) AS
 $$
 DECLARE
-    Comando VARCHAR(500):='SELECT uri FROM f.foto WHERE';
-    cursore CURSOR FOR SELECT parola, uri FROM f.foto NATURAL JOIN f.tagfoto;
-    uri_attuale f.foto.uri%TYPE;
-    uri_prev f.foto.uri%TYPE;
-    i_parola f.hashtag.parola%TYPE;
-    t_parola f.hashtag.parola%TYPE;
-    n_paroleinput INTEGER:=regexp_count(input, '@')+1;
-    n_paroleTable INTEGER:= (SELECT count(*) FROM f.foto NATURAL JOIN f.tagfoto);
-    match INTEGER:=0;
-    varend VARCHAR(8):=' AND ';
-    BEGIN
-        OPEN cursore;
-        FOR i IN 1..n_paroleTable LOOP
+    Comando       VARCHAR(500) := 'SELECT uri FROM f.foto WHERE';
+    cursore CURSOR FOR SELECT parola, uri
+                       FROM f.foto
+                                NATURAL JOIN f.tagfoto;
+    uri_attuale   f.foto.uri%TYPE;
+    uri_prev      f.foto.uri%TYPE;
+    i_parola      f.hashtag.parola%TYPE;
+    t_parola      f.hashtag.parola%TYPE;
+    n_paroleinput INTEGER      := regexp_count(input, '@') + 1;
+    n_paroleTable INTEGER      := (SELECT count(*)
+                                   FROM f.foto
+                                            NATURAL JOIN f.tagfoto);
+    match         INTEGER      := 0;
+    varend        VARCHAR(8)   := ' AND ';
+BEGIN
+    OPEN cursore;
+    FOR i IN 1..n_paroleTable
+        LOOP
             FETCH cursore INTO t_parola, uri_attuale;
             RAISE NOTICE 'uriattuale{%}, uriprev{%}', uri_attuale, uri_prev;
-            if(uri_attuale<>uri_prev) THEN match=0; END IF;
-            FOR j IN 1..n_paroleinput LOOP
-                i_parola=split_part(input, '@', j);
-                if(i_parola=t_parola) THEN
-                    RAISE NOTICE 'parolatable{%} parolainput{%}', t_parola, i_parola;
-                    match=match+1;
-                end if;
-                if(n_paroleinput=match) THEN
-                    RAISE NOTICE 'n_parole(%), match(%)', n_paroleinput, match;
-                    Comando= Comando || 'uri=' || '''' ||uri_attuale || '''';
-                    BREAK;
-                end if;
+            if (uri_attuale <> uri_prev) THEN match = 0; END IF;
+            FOR j IN 1..n_paroleinput
+                LOOP
+                    i_parola = split_part(input, '@', j);
+                    if (i_parola = t_parola) THEN
+                        RAISE NOTICE 'parolatable{%} parolainput{%}', t_parola, i_parola;
+                        match = match + 1;
+                    end if;
+                    if (n_paroleinput = match) THEN
+                        RAISE NOTICE 'n_parole(%), match(%)', n_paroleinput, match;
+                        Comando = Comando || 'uri=' || '''' || uri_attuale || '''';
+                        BREAK;
+                    end if;
                 end loop;
-            uri_prev=uri_attuale;
+            uri_prev = uri_attuale;
 
-            END LOOP;
-        CLOSE cursore;
+        END LOOP;
+    CLOSE cursore;
 
-        RAISE NOTICE '%', Comando;
-        END
+    RAISE NOTICE '%', Comando;
+END
 
-    $$
-language plpgsql;
+$$
+    language plpgsql;
 
 CALL f.prova1('prova11@prova12');
 
-SELECT uri FROM f.foto WHERE uri='uri1' AND uri='uri1' AND uri='uri2'
+SELECT uri
+FROM f.foto
+WHERE uri = 'uri1'
+  AND uri = 'uri1'
+  AND uri = 'uri2'
