bf99ad3836567764043a2515fc98588c7a83fa07
Mario Penna <mario.penna00@gmail.com>
2022-12-19 21:33:40 +0100

    Sistemazione MARIO

--- F1_E3-E4 Riviste ed Articoli/Funzione_1(ES4)MARIO.sql
+++ F1_E3-E4 Riviste ed Articoli/Funzione_1(ES4)MARIO.sql
@@ -1,6 +1,6 @@
---START
+--ES4 MARIO PENNA
 
-CREATE OR REPLACE PROCEDURE r.pro(string VARCHAR(500)) AS $$
+CREATE OR REPLACE FUNCTION r.pro(string VARCHAR(500)) RETURNS VARCHAR(500) AS $$
     DECLARE
         numParoleSTR INTEGER;
         parolaSTR r.descrizione.parola%TYPE;
@@ -25,8 +25,7 @@ CREATE OR REPLACE PROCEDURE r.pro(string VARCHAR(500)) AS $$
             FOR j in 1..numParoleSTR LOOP
                 parolaSTR = split_part(string, '@', j);
                 RAISE NOTICE 'Parola Attuale Stringa: {%}', parolaSTR;
-                IF EXISTS (SELECT *
-                           FROM r.descrizione as d
+                IF EXISTS (SELECT * FROM r.descrizione as d
                            WHERE d.doi=currentDOI AND d.parola=parolaSTR) THEN
                 match = match +1;
                 RAISE NOTICE 'Match: {%}', match;
@@ -39,7 +38,8 @@ CREATE OR REPLACE PROCEDURE r.pro(string VARCHAR(500)) AS $$
         end loop;
         RAISE NOTICE '----------------';
         RAISE NOTICE 'Output: {%}', output;
+        RETURN output;
     END;
 $$ LANGUAGE plpgsql;
 
-CALL r.pro('prova2');
+SELECT r.pro('prova2');
