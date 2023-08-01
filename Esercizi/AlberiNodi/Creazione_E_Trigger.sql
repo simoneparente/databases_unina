cc9e536b567738478e63929b0aca8017f19bbdb2
Simone Parente <parentesimone.01@gmail.com>
2023-08-01 13:22:25 +0200

    Esercizio Alberi
    Aggiunta traccia, modificato il disegno e modifiche minori del file .sql

--- Esercizi/AlberiNodi/Creazione_E_Trigger.sql
+++ Esercizi/AlberiNodi/Creazione_E_Trigger.sql
@@ -1,12 +1,10 @@
 --MARIO PENNA E SIMONE PARENTE MARTONE
-
+DROP SCHEMA IF EXISTS e CASCADE;
 CREATE SCHEMA e;
-
-
 
 CREATE TABLE e.Nodo
 (
-    CodA  INTEGER,  --FK
+    CodA  INTEGER, --FK
     CodN  INTEGER, --PK
     Label INTEGER,
 
@@ -19,7 +17,7 @@ CREATE TABLE e.Albero
     root INTEGER,
 
     CONSTRAINT Albero_PK PRIMARY KEY (CodA),
-    CONSTRAINT Nodo_PK FOREIGN KEY (root) REFERENCES e.Nodo(CodN)
+    CONSTRAINT Nodo_PK FOREIGN KEY (root) REFERENCES e.Nodo (CodN)
 );
 
 CREATE TABLE e.Arco
@@ -30,64 +28,61 @@ CREATE TABLE e.Arco
     Figlio  INTEGER, --FK
 
     CONSTRAINT Arco_PK PRIMARY KEY (CodArco),
-    CONSTRAINT Albero_FK FOREIGN KEY (CodA) REFERENCES e.Albero(CodA),
-    CONSTRAINT Padre_FK FOREIGN KEY (Padre) REFERENCES e.Nodo(CodN),
-    CONSTRAINT Figlio_FK FOREIGN KEY (Figlio) REFERENCES e.Nodo(CodN)
+    CONSTRAINT Albero_FK FOREIGN KEY (CodA) REFERENCES e.Albero (CodA),
+    CONSTRAINT Padre_FK FOREIGN KEY (Padre) REFERENCES e.Nodo (CodN),
+    CONSTRAINT Figlio_FK FOREIGN KEY (Figlio) REFERENCES e.Nodo (CodN)
 );
 
 ALTER TABLE e.Nodo
-ADD CONSTRAINT  Albero_FK FOREIGN KEY (CodA) REFERENCES e.Albero(CodA);
+    ADD CONSTRAINT Albero_FK FOREIGN KEY (CodA) REFERENCES e.Albero (CodA);
 
 INSERT INTO e.Albero(coda)
-(
-        values (1),
-               (2),
-               (3)
-);
+    (values (1),
+            (2),
+            (3));
 
 INSERT INTO e.Nodo(codn, coda, label) --nel disegno i numeri dei nodi corrispondono ai label
-(           --albero 1
-    values (11,1,1),
-           (12,1,2),
-           (13,1,3),
-           (14,1,4),
-           (15,1,5),
-           (16,1,6),
-           (17,1,7),
-            --albero 2
-           (210,2,10),
-           (211,2,11),
-           (212,2,12),
-           (23,2,3),
-           (26,2,6),
-           (27,2,7),
-           (214,2,14),
-           (215,2,15),
-           (216,2,16),
-            --albero 3
-           (321,3,21),
-           (312,3,12),
-           (315,3,15),
-           (316,3,16),
-           (323,3,23),
-           (327,3,27),
-           (328,3,28)
-);
+    ( --albero 1
+        values (11, 1, 1),
+               (12, 1, 2),
+               (13, 1, 3),
+               (14, 1, 4),
+               (15, 1, 5),
+               (16, 1, 6),
+               (17, 1, 7),
+               --albero 2
+               (210, 2, 10),
+               (211, 2, 11),
+               (212, 2, 12),
+               (23, 2, 3),
+               (26, 2, 6),
+               (27, 2, 7),
+               (214, 2, 14),
+               (215, 2, 15),
+               (216, 2, 16),
+               --albero 3
+               (321, 3, 21),
+               (312, 3, 12),
+               (315, 3, 15),
+               (316, 3, 16),
+               (323, 3, 23),
+               (327, 3, 27),
+               (328, 3, 28));
 
 UPDATE e.Albero
 SET root=11
-WHERE coda=1;
+WHERE CodA = 1;
 
 UPDATE e.albero
 SET root=210
-WHERE coda=2;
+WHERE CodA = 2;
 
 UPDATE e.albero
 SET root=321
-WHERE coda=3;
+WHERE CodA = 3;
 
-INSERT INTO e.Arco(coda, codarco, padre, figlio)
-( --ALBERO 1
+INSERT INTO e.Arco(CodA, codarco, padre, figlio)
+    ( --ALBERO 1
         values (1, 112, 11, 12),
                (1, 113, 11, 13),
                (1, 124, 12, 14),
@@ -109,95 +104,92 @@ INSERT INTO e.Arco(coda, codarco, padre, figlio)
                (3, 31215, 312, 315),
                (3, 31216, 312, 316),
                (3, 32327, 323, 327),
-               (3, 32328, 323, 328)
-);
+               (3, 32328, 323, 328));
 
+--Scrivere una funzione che prenda in input un albero e un nodo e restituisca la somma dei label dei nodi del cammino dalla radice al nodo.
 CREATE OR REPLACE FUNCTION e.somma_nodi(albero e.Albero.CodA%TYPE, nodo_input e.Nodo.CodN%TYPE)
     RETURNS INT
 AS
-  $$
-    DECLARE
-        somma_nodi INTEGER:=0;
-        root_a e.Albero.root%TYPE;
-        padre e.Arco.padre%TYPE;
-        cursore e.Arco.figlio%TYPE:=nodo_input;
-        labelv e.Nodo.label%TYPE:=0; --variabile label
-        BEGIN
-            SELECT root, N.label  --trovo radice dell'albero e relativo label
-                into root_a, labelv --e lo salvo in due variabili distinte
-                FROM e.Albero AS A NATURAL JOIN e.Nodo AS N
-                WHERE albero=A.coda;
-            somma_nodi=somma_nodi+labelv;  --sommo il label della radice
-
-            WHILE cursore!=root_a LOOP      --salgo di nodo in nodo fino a raggiungere la radice precedentemente trovata
-                SELECT A.padre
-                    INTO padre
-                    FROM e.Nodo AS N NATURAL JOIN e.Arco AS A
-                    WHERE figlio=cursore;
-                SELECT N.label
-                    INTO labelv
-                    FROM e.Nodo AS N
-                    WHERE cursore=N.codn;
-                somma_nodi:=somma_nodi+labelv; --sommo i label dei nodi
-                cursore=padre;
-
-            END LOOP;
-
-            RETURN somma_nodi;
-        END
 $$
-
-LANGUAGE plpgsql;
-
-SELECT e.somma_nodi(2,27);
-
-
-CREATE or REPLACE FUNCTION e.max_cammino (cod_albero e.albero.codA%TYPE)
-RETURNS INT
-AS
-    $$
-    DECLARE
-    foglia e.Nodo.CodN%TYPE; --variabile in cui conserviamo il CodN di una foglia
-    max INTEGER := 0;          --max valore del percorso dalla foglia alla radice
-    i   INTEGER := 0;            --indice per loop
-    n   INTEGER := 0;            --numero di foglie dell'albero (da usare come max del loop)
-    cursore CURSOR FOR       --cursore che scorre tutti i nodi senza figli(foglie)
-        (
-            SELECT N.CodN           --tutti i nodi
-                FROM e.Nodo AS N
-                WHERE coda=cod_albero
-            EXCEPT                  --meno
-            SELECT A.padre          --nodi con figli
-                FROM e.Arco AS A
-        );
-
+DECLARE
+    somma_nodi INTEGER            := 0;
+    root_a     e.Albero.root%TYPE;
+    padre      e.Arco.padre%TYPE;
+    cursore    e.Arco.figlio%TYPE := nodo_input;
+    labelv     e.Nodo.label%TYPE  := 0; --variabile label
 BEGIN
-    n:=(
-        SELECT COUNT(*)     --numero di foglie dell'albero
-            FROM (SELECT N.CodN
-                    FROM e.Nodo AS N
-                    WHERE coda = cod_albero
-                    EXCEPT
-                    SELECT A.padre
-                        FROM e.Arco AS A
-                  )     AS Q
-        );
+    SELECT root, N.label --trovo radice dell'albero e relativo label
+    into root_a, labelv --e lo salvo in due variabili distinte
+    FROM e.Albero AS A
+             NATURAL JOIN e.Nodo AS N
+    WHERE albero = A.coda;
+    somma_nodi = somma_nodi + labelv; --sommo il label della radice
 
-        OPEN cursore;
-
-        WHILE i<n LOOP
-            FETCH cursore INTO foglia;
-            IF(max < e.somma_nodi(cod_albero, foglia)) THEN
-                max = e.somma_nodi(cod_albero, foglia);
-            END IF;
-            i:=i+1;
+    WHILE cursore != root_a
+        LOOP
+            --salgo di nodo in nodo fino a raggiungere la radice precedentemente trovata
+            SELECT A.padre INTO padre
+            FROM e.Nodo AS N
+                     NATURAL JOIN e.Arco AS A
+            WHERE figlio = cursore;
+            SELECT N.label INTO labelv
+            FROM e.Nodo AS N
+            WHERE cursore = N.codn;
+            somma_nodi := somma_nodi + labelv; --sommo i label dei nodi
+            cursore = padre;
         END LOOP;
 
-        CLOSE cursore;
-        RETURN max;
-    END;
+    RETURN somma_nodi;
+END
+$$
+    LANGUAGE plpgsql;
+
+SELECT e.somma_nodi(2, 23);
+
+
+CREATE or REPLACE FUNCTION e.max_cammino(cod_albero e.albero.codA%TYPE)
+    RETURNS INT
+AS
+$$
+DECLARE
+    foglia e.Nodo.CodN%TYPE; --variabile in cui conserviamo il CodN di una foglia
+    max    INTEGER := 0; --max valore del percorso dalla foglia alla radice
+    i      INTEGER := 0; --indice per loop
+    n      INTEGER := 0; --numero di foglie dell'albero (da usare come max del loop)
+    cursore CURSOR FOR --cursore che scorre tutti i nodi senza figli(foglie)
+        (SELECT N.CodN --tutti i nodi
+         FROM e.Nodo AS N
+         WHERE coda = cod_albero
+         EXCEPT --meno
+         SELECT A.padre --nodi con figli
+         FROM e.Arco AS A);
+
+BEGIN
+    n := (SELECT COUNT(*) --numero di foglie dell'albero
+          FROM (SELECT N.CodN
+                FROM e.Nodo AS N
+                WHERE coda = cod_albero
+                EXCEPT
+                SELECT A.padre
+                FROM e.Arco AS A) AS Q);
+
+    OPEN cursore;
+
+    WHILE i < n
+        LOOP
+            FETCH cursore INTO foglia;
+            IF (max < e.somma_nodi(cod_albero, foglia)) THEN
+                max = e.somma_nodi(cod_albero, foglia);
+            END IF;
+            i := i + 1;
+        END LOOP;
+
+    CLOSE cursore;
+    RETURN max;
+END;
 
 $$
-LANGUAGE plpgsql;
+    LANGUAGE plpgsql;
 
-SELECT e.max_cammino(1);
+--Dato in input un albero, restituisce la somma massima dei label dei nodi di un cammino dalla radice ad una foglia.
+SELECT e.max_cammino(3);
